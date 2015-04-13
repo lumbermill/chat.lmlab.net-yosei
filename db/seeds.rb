2f8cd00377bc485a5a8a299224bcd7d2c5220507
sohei yagi <soheiyagi@gmail.com>
2015-04-13 09:41:56 +0900

    シードデータの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Chat.create(
+  user: "itou",
+  message: "素数を求めなさい。" 
+  )
+
+Chat.create(
+  user: "yagi",
+  message: "先生、難しいです。これは物理の授業では？" 
+  )
